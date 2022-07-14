45c5caf95961d73baabca52fc0c361890f2d2fd3
Kirubel A <kirubelabr@gmail.com>
2022-07-14 18:08:32 +0300

    Initial project setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1 className='text-3xl font-bold underline'>Vite + React + TypeScript + Antd + TailwindCSS + SASS + Vitest</h1>
+    </div>
+  )
+}
+
+export default App
